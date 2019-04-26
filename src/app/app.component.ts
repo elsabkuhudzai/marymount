cbdb438daaa20d2a1d79fdcd46997c4836ca3408
Marlon Miles MG <magonjomarlon@gmail.com>
2019-04-26 09:32:48 +0200

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+@Component({
+  selector: 'app-root',
+  templateUrl: './../../index.html',
+  styleUrls: ['./../../css/style.css']
+})
+export class AppComponent {
+  title = 'mary-mount';
+}
